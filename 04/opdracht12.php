ce63ac728b830c6c2e9b5b7ba062821e94a6741d
Ben Adams <adams.ben1996@gmail.com>
2026-09-24 16:34:04 +0200

    Les 05: Operators, expressies & scope

--- 04/opdracht12.php
+++ 04/opdracht12.php
@@ -1,6 +1,6 @@
 <?php
 include("functions.php");
 show_header(); //Q: werkt ook als ik deze functie weg laat en dus geen 'valid' html krijg
-show_content();
+show_content(); //Q: word indent automatisch gedaan wanneer je het zo echo't.
 show_footer();
 ?>
